3d448af03b4ee792f66ed92641088522a009170b
Mo'men Gharib <momengharib22@hotmail.com>
2023-02-05 14:59:39 +0200

    First commit 2 pages.

---
+++ pages/TextToImage.js
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import {
+  View,
+  TextInput,
+  Image,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  Touchable,
+  TouchableNativeFeedback,
+} from "react-native";
+
+const TextToImage = () => {
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
+
+  const generateImage = async () => {
+    // code to call the DALL-E API and get the generated image
+    const response = await fetch(
+      `https://api.openai.com/v1/images/generations`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer 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`,
+        },
+        body: JSON.stringify({
+          model: "image-alpha-001",
+          prompt: text,
+          num_images: 1,
+          size: "1024x1024",
+        }),
+      }
+    );
+
+    const data = await response.json();
+    setImage(data.data[0].url);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Imagination is free!"
+        onChangeText={(text) => setText(text)}
+        value={text}
+      />
+      <TouchableNativeFeedback>
+        <Image source={{ uri: image }} style={styles.image} />
+      </TouchableNativeFeedback>
+
+      <Button onPress={generateImage} title="Generate Image"></Button>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    width: 200,
+    borderColor: "gray",
+    borderWidth: 1,
+    margin: 10,
+    padding: 10,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    margin: 10,
+  },
+});
+
+export default TextToImage;
