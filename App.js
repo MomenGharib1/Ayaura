cf3e45d859a8811a237df6b2f51c08b44e9cb055
Mo'men Gharib <momengharib22@hotmail.com>
2023-02-06 15:30:34 +0200

    Revert "test"
    This reverts commit 1fccb6e113714fce9d38cfc19e07bc10f56f37b3.

---
+++ App.js
@@ -0,0 +1,12 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import TextToImage from "./pages/TextToImage";
+import Home from "./pages/Home";
+import Navigator from "./routes/homeStack";
+import axios from "axios";
+
+export default function App() {
+  const a = 5555435;
+  axios.defaults.baseURL = "http://localhost:3001/";
+  return <Navigator />;
+}
---
+++ pages/TextToImage.js
@@ -0,0 +1,92 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+import {
+  View,
+  TextInput,
+  Image,
+  StyleSheet,
+  Button,
+  SafeAreaView,
+  Touchable,
+  TouchableNativeFeedback,
+  Text,
+} from "react-native";
+
+const TextToImage = () => {
+  const [text, setText] = useState("");
+  const [image, setImage] = useState(null);
+
+  const generateImage = async () => {
+    // code to call the DALL-E API and get the generated image
+    const response = await fetch(
+      `https://api.openai.com/v1/images/generations`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer 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`,
+        },
+        body: JSON.stringify({
+          model: "image-alpha-001",
+          prompt: text,
+          num_images: 1,
+          size: "1024x1024",
+        }),
+      }
+    );
+
+    const data = await response.json();
+    setImage(data.data[0].url);
+
+    const imageData = {
+      url: data.data[0].url,
+      name: "image-" + new Date().getTime(),
+      prompt: text,
+    };
+    const response2 = await axios.post("/images", imageData);
+    const data2 = await response2.json();
+    console.log(data2.message);
+  };
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Imagination is free!"
+        onChangeText={(text) => setText(text)}
+        value={text}
+      />
+      <TouchableNativeFeedback>
+        <Image source={{ uri: image }} style={styles.image} />
+      </TouchableNativeFeedback>
+
+      <Button onPress={generateImage} title="Generate Image"></Button>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  input: {
+    minHeight: 40,
+    width: 200,
+    borderColor: "#3C84AB",
+    color: "black",
+    borderRadius: 10,
+    borderWidth: 2,
+    margin: 10,
+    padding: 10,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    margin: 10,
+  },
+});
+
+export default TextToImage;
