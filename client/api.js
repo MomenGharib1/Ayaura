16a00e518c5f96855aa1fb62f41dab105d8a428c
Mo'men Gharib <94765709+MomenGharib1@users.noreply.github.com>
2023-02-16 14:29:39 +0200

    Delete client directory

--- client/api.js
+++
@@ -1,8 +0,0 @@
-export const getPosts = async () =>
-  await fetch("https://ayaura.azurewebsites.net/getImages")
-    .then((res) => {
-      if (res.ok) {
-        return res.json();
-      }
-    })
-    .then((jsonRes) => jsonRes);
